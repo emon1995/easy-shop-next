b76b47f1c3b57ddcee99afa6d77281155d8ef063
emon1995 <emonhossain995@gmail.com>
2023-07-17 19:27:11 +0600

    Basic setup footer & navbar dark mode etc.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    images: [
+        {
+            protocol: "https",
+            hostname: "lh3.gooleusercontent.com"
+        },
+        {
+            protocol: "https",
+            hostname: "i.ibb.co"
+        }
+    ]
+}
 
 module.exports = nextConfig
---
+++ src/app/blogs/pages.jsx
@@ -0,0 +1,11 @@
+
+
+const BlogsPage = () => {
+    return (
+        <div>
+            <h1>BlogsPage</h1>
+        </div>
+    );
+};
+
+export default BlogsPage;
